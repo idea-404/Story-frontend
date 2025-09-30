7c2451e403f47ba329f605671209df1ad2386e77
yeondon <s25030@gsm.hs.kr>
2025-09-30 18:15:18 +0900

    init : React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
