96aefc83a3bf3e7c9eb37425d44494deea9b5bae
yeondon <s25030@gsm.hs.kr>
2025-10-02 08:34:22 +0900

    chore : @ 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss({ config: './tailwind.config.cjs' })],
-})
+  plugins: [react(), tailwindcss({ config: "./tailwind.config.cjs" })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
