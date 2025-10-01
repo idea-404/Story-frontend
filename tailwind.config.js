7f5dceac9069d896d01a0977602d9a336358969e
yeondon <s25030@gsm.hs.kr>
2025-10-01 13:39:36 +0900

    chore : 파일 삭제

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 // tailwind.config.cjs
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -8,15 +8,15 @@ module.exports = {
           main1: "#AE89FF",
           main2: "#AE89FF",
           main3: "#DAC8FF",
-          red: "#f84f4f",
-          blue: "#525AFF",
-          green: "#37CC70",
-          green2: "#8AFF8C",
-          yellow: "#FFFF65",
         },
+        "custom-red": "#f84f4f",
+        "custom-blue": "#525AFF",
+        "custom-green": "#37CC70",
+        "custom-green2": "#8AFF8C",
+        "custom-yellow": "#FFFF65",
         background: "#FAFAFA",
       },
     },
-    plugins: [],
   },
+  plugins: [],
 };
