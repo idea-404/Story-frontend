560342f29d7ec4a6ccaee696cfbc20706d9db522
yeondon <s25030@gsm.hs.kr>
2025-10-05 01:37:22 +0900

    feat : Header 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes></Routes>
     </BrowserRouter>
   );
