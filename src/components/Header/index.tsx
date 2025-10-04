c8fed2df9dfaa90bbb6ec4aad1e34861c364defd
yeondon <s25030@gsm.hs.kr>
2025-10-04 20:45:16 +0900

    style : Header 디자인 완성

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
-import { Logo, Person } from "@/assets";
+import { Logo, Person, Pen } from "@/assets";
 
 const Header = () => {
   return (
     <header className="flex justify-center">
-      <div className="flex w-[37.5rem] justify-between content-center">
+      <div className="flex w-[37.5rem] h-[5.375rem] justify-between items-center">
         <div>
           <Logo />
         </div>
-        <div className="flex ">
-          <div>글작성</div>
+        <div className="flex gap-[1.25rem]">
+          <div className="flex items-center gap-[0.625rem] py-[0.375rem] px-[0.875rem] border-[0.025rem] rounded-[1.5rem]">
+            글작성 <Pen />
+          </div>
           <Person />
         </div>
       </div>
