3c245986a88bb7a3f7cf0e9be590e06f3d9dd0a7
yeondon <s25030@gsm.hs.kr>
2025-10-06 13:50:22 +0900

    feat : endpoint 가 signin 일 시 header 가 일부분만 출력

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Header } from "./components";
 
 function App() {
+  const location = useLocation();
+  const isSignIn = location.pathname === "/signin";
   return (
-    <BrowserRouter>
-      <Header />
+    <>
+      <Header EndPoint={isSignIn} />
       <Routes></Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
 import { Logo, Person, Pen } from "@/assets";
 
-const Header = () => {
+const Header = ({ EndPoint }: { EndPoint: boolean }) => {
   return (
     <header className="flex justify-center">
       <div className="flex w-[37.5rem] h-[5.375rem] justify-between items-center">
         <div>
           <Logo />
         </div>
-        <div className="flex gap-[1.25rem]">
+        <div className={EndPoint ? "hidden" : "flex gap-[1.25rem]"}>
           <div className="flex items-center gap-[0.625rem] py-[0.375rem] px-[0.875rem] border-[0.025rem] rounded-[1.5rem]">
             글작성 <Pen />
           </div>
