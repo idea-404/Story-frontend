771307a2ff4edca4f7eb6fd63271f635316ac523
서연 <s25037@gsm.hs.kr>
2025-10-11 01:22:46 +0900

    fix: index.ts 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
-export { default as Header } from "./Header";
+export { default as Header } from './Header';
+export { default as MainCard } from './MainCard';
+export { default as MainHeader } from './MainHeader';
