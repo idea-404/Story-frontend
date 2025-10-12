2d96ee2a1b6ca74f09fe2667491caac3cf51b1ce
yeondon <s25030@gsm.hs.kr>
2025-10-13 08:13:04 +0900

    feat : kakao 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,4 @@ export { default as Logo } from "./Logo";
 export { default as Person } from "./Person";
 export { default as Pen } from "./Pen";
 export { default as Google } from "./Google";
+export { default as Kakao } from "./Kakao";
