35da9c273b40e0d2679e87bd1d9ed0d31c6f15e5
yeondon <s25030@gsm.hs.kr>
2025-10-13 11:04:48 +0900

    feat : 기본 login 스타일

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Header } from "./components";
+import { Login } from "./page";
 
 function App() {
   const location = useLocation();
@@ -8,7 +9,9 @@ function App() {
   return (
     <>
       <Header EndPoint={isSignIn} />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
---
+++ src/page/Login/index.tsx
@@ -0,0 +1,16 @@
+const Login = () => {
+  return (
+    <main className="flex justify-center">
+      <div className="w-[32.5rem] ">
+        <div className="my-[5.5rem] text-[2rem] font-medium">
+          당신의
+          <br />
+          <span className="text-primary-main1">Story</span>를
+          <br />
+          시작해 보세요!
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Login;
