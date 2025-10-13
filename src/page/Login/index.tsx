3ec37862db05ca8647c603f1688d6671a8e0378c
yeondon <s25030@gsm.hs.kr>
2025-10-13 15:59:11 +0900

    feat : 기본 페이지 구성

--- src/page/Login/index.tsx
+++ src/page/Login/index.tsx
@@ -1,13 +1,31 @@
+import { Google, Kakao, Line2 } from "@/assets";
+
 const Login = () => {
   return (
-    <main className="flex justify-center">
-      <div className="w-[32.5rem] ">
-        <div className="my-[5.5rem] text-[2rem] font-medium">
-          당신의
-          <br />
-          <span className="text-primary-main1">Story</span>를
-          <br />
-          시작해 보세요!
+    <main className="flex items-center flex-col">
+      <div className="w-[32.5rem] my-[5.5rem] text-[2rem] font-medium">
+        당신의
+        <br />
+        <span className="text-primary-main1">Story</span>를
+        <br />
+        시작해 보세요!
+      </div>
+      <div className="w-[32.5rem] h-[2.5rem] flex flex-col justify-center gap-4">
+        <div className="flex items-center gap-4">
+          <Line2 /> 소셜 로그인 <Line2 />
+        </div>
+
+        <div></div>
+
+        <div className="flex flex-col gap-[0.875rem]">
+          <button className="flex gap-[1.625rem] w-[32.5rem] h-[3.375rem] justify-center items-center bg-[#EFF0F2] rounded-[0.625rem]">
+            <Google />
+            <div>구글 계정으로 로그인</div>
+          </button>
+          <button className="flex gap-[0.5rem] w-[32.5rem] h-[3.375rem] justify-center items-center bg-[#F7E44C] rounded-[0.625rem]">
+            <Kakao />
+            <div>카카오 계정으로 로그인</div>
+          </button>
         </div>
       </div>
     </main>
