5d42d970f87cc6be9b65ca807aa27416e3019ccd
서연 <s25037@gsm.hs.kr>
2025-10-21 20:16:59 +0900

    style: 검색 아이콘 위치 이동

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,13 +2,15 @@ import { SearchPurple } from "@/assets";
 
 const Search = () => {
   return (
-    <div className="flex items-center w-full max-w-sm px-3 py-2 bg-white border border-gray-300 rounded-full shadow-sm focus-within:border-purple-400">
-      <SearchPurple />
-      <input
-        type="text"
-        placeholder="검색어를 입력하세요"
-        className="w-full outline-none text-gray-700 placeholder-gray-400"
-      />
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex items-center w-full max-w-sm px-3 py-2 bg-white border border-gray-700 rounded-full  focus-within:border-purple-400">
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요"
+          className="w-full outline-none text-gray-500 placeholder-gray-400"
+        />
+        <SearchPurple />
+      </div>
     </div>
   );
 };
