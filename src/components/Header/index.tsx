2665bbb71479b1eb7c4d3ff27e5951e6cd0ab43b
yeondon <s25030@gsm.hs.kr>
2025-10-23 18:27:41 +0900

    refactor : endpoint 확인 방법 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
 import { Logo, Person, Pen } from "@/assets";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEndpointStore } from "@/store/useEndpointStore";
 
-const Header = ({ EndPoint }: { EndPoint: boolean }) => {
+const Header = () => {
   const navigate = useNavigate();
+  const { endpoint } = useEndpointStore();
   return (
     <header className="flex justify-center border-b-[0.125rem] border-[#EFF0F2]">
       <div className="flex w-[37.5rem] h-[5.375rem] justify-between items-center">
         <div onClick={() => navigate("/")}>
           <Logo />
         </div>
-        <div className={EndPoint ? "hidden" : "flex gap-[1.25rem]"}>
+        <div
+          className={endpoint === "/signin" ? "hidden" : "flex gap-[1.25rem]"}
+        >
           <div
             className="flex items-center gap-[0.625rem] py-[0.375rem] px-[0.875rem] border-[0.025rem] rounded-[1.5rem] font-medium cursor-default
 "
