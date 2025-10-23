e586753c2e500fa7fc67863cfb93ed2fd2de7b33
서연 <s25037@gsm.hs.kr>
2025-10-23 23:23:46 +0900

    style: 검색 아이콘 위치 조정

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -6,10 +6,12 @@ const Search = () => {
       <div className="flex items-center max-w-sm px-3 py-2 w-[460px] h-[46px]  bg-white border-[0.5px] border-gray-700 rounded-2xl  focus-within:border-purple-400">
         <input
           type="text"
-          placeholder="검색어를 입력하세요"
+          placeholder="검색어를 입력하세요."
           className="w-full outline-none text-gray-500 placeholder-gray-400"
         />
-        <SearchPurple />
+        <button className="relative right-2">
+          <SearchPurple />
+        </button>
       </div>
     </div>
   );
