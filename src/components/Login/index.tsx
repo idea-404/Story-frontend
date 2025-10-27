0de9ef1ca97cdda3c391d7ec4d37caf3daf4e870
yeondon <s25030@gsm.hs.kr>
2025-10-27 09:15:02 +0900

    feat : 디자인 설정

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -8,9 +8,11 @@ const Logincomponents = () => {
         <input
           type="email"
           placeholder="이메일로 로그인"
-          className="w-full border-[0.0625rem] h-[3.5rem]"
+          className="w-full border-[0.0625rem] h-[3.5rem] rounded-[0.625rem] px-[1.25rem]"
         />
-        <button className="w-full h-[3.5rem] bg-primary-main3">로그인</button>
+        <button className="w-full h-[3.5rem] bg-primary-main3 text-white rounded-[0.625rem] text-[1.375rem] font-bold ">
+          로그인
+        </button>
       </div>
       <div className="flex gap-[0.3125rem] text-primary-main1 justify-end">
         <div>Story가 처음이라면?</div>
