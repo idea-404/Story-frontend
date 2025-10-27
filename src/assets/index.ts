317a054b8a3127e4de3da638213fc6b3363d0b14
yeondon <s25030@gsm.hs.kr>
2025-10-27 14:11:32 +0900

    feat : signin 프로필 사진 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 export { default as Logo } from "./Logo";
 export { default as Person } from "./Person";
 export { default as Pen } from "./Pen";
+export { default as SigninPerson } from "./SigninPerson";
