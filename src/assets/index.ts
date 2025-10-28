aecd06970b54618519e0fd7af25675799ebfb7a0
yeondon <s25030@gsm.hs.kr>
2025-10-28 17:46:18 +0900

    feat : star 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,4 @@ export { default as Logo } from "./Logo";
 export { default as Person } from "./Person";
 export { default as Pen } from "./Pen";
 export { default as SigninPerson } from "./SigninPerson";
+export { default as Star } from "./Star";
