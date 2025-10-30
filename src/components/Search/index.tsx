2aaab54263c328cfa24104420b6f48e7b389d072
서연 <s25037@gsm.hs.kr>
2025-10-30 17:55:42 +0900

    feat: promise 삭제

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -3,10 +3,16 @@ import { SearchPurple } from "@/assets";
 interface SearchProps {
   value: string;
   onChange: (value: string) => void;
-  onSearch: () => Promise<void>;
+  onSearch: () => void;
 }
 
 const Search = ({ value, onChange, onSearch }: SearchProps) => {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSearch();
+    }
+  };
+
   return (
     <div className="flex justify-center items-center h-screen -mt-82">
       <div className="flex items-center max-w-sm px-3 py-2 w-[460px] h-[46px] bg-white border-[0.5px] border-gray-700 rounded-2xl focus-within:border-purple-400">
@@ -14,6 +20,7 @@ const Search = ({ value, onChange, onSearch }: SearchProps) => {
           type="text"
           value={value}
           onChange={(e) => onChange(e.target.value)}
+          onKeyDown={handleKeyDown}
           placeholder="검색어를 입력하세요."
           className="w-full outline-none text-gray-500 placeholder-gray-400"
         />
