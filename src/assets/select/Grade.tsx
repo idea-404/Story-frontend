f4707a2cd3a94b940a7ca0d20598dcd013dc61e4
yeondon <s25030@gsm.hs.kr>
2025-11-06 23:51:26 +0900

    feat : Grade 값 불러오기

--- src/assets/select/Grade.tsx
+++ src/assets/select/Grade.tsx
@@ -1,34 +1,41 @@
-import { useFormContext } from "react-hook-form";
+import { useFormContext, Controller } from "react-hook-form";
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
 
 const Grade = () => {
+  const { control } = useFormContext();
+
   return (
-    <Select>
-      <SelectTrigger className="w-[8.5rem] h-[3.5rem] text-[1.375rem] text-[#828387] rounded-[0.75rem]">
-        <SelectValue placeholder="학년" />
-      </SelectTrigger>
-      <SelectContent className="bg-white border-0 text-[1.375rem] font-normal align-center max-h-[11.625rem]">
-        <SelectGroup className="h-[11.625rem] mb-[0.75rem]">
-          <SelectItem value="1" className="text-[1.375rem] mt-[0.25rem] ">
-            1학년
-          </SelectItem>
-          <SelectItem value="2" className=" text-[1.375rem] mt-[0.25rem]">
-            2학년
-          </SelectItem>
-          <SelectItem value="3" className="text-[1.375rem] mt-[0.25rem]">
-            3학년
-          </SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <Controller
+      name="grade"
+      control={control}
+      render={({ field }) => (
+        <Select onValueChange={field.onChange} value={field.value}>
+          <SelectTrigger className="w-[8.5rem] h-[3.5rem] text-[1.375rem] text-[#828387] rounded-[0.75rem]">
+            <SelectValue placeholder="학년" />
+          </SelectTrigger>
+          <SelectContent className="bg-white border-0 text-[1.375rem] font-normal align-center max-h-[11.625rem]">
+            <SelectGroup className="h-[11.625rem] mb-[0.75rem]">
+              <SelectItem value="1" className="text-[1.375rem] mt-[0.25rem] ">
+                1학년
+              </SelectItem>
+              <SelectItem value="2" className=" text-[1.375rem] mt-[0.25rem]">
+                2학년
+              </SelectItem>
+              <SelectItem value="3" className="text-[1.375rem] mt-[0.25rem]">
+                3학년
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      )}
+    />
   );
 };
 export default Grade;
