12da57cdb030383f39632c57b332a0c39aca6998
yeondon <s25030@gsm.hs.kr>
2025-11-06 10:30:07 +0900

    feat : major 수정

--- src/assets/select/Major.tsx
+++ src/assets/select/Major.tsx
@@ -11,22 +11,31 @@ import {
 const Major = () => {
   return (
     <Select>
-      <SelectTrigger className="w-[8.5rem] h-[3.5rem] text-[1.375rem] text-[#828387] rounded-[0.75rem]">
-        <SelectValue placeholder="반" />
+      <SelectTrigger className="w-[31.25rem] h-[3.5rem] text-[1.375rem] text-[#828387] rounded-[0.75rem]">
+        <SelectValue placeholder="전공을 선택해 주세요." />
       </SelectTrigger>
       <SelectContent className="bg-white border-0 text-[1.375rem] font-normal align-center max-h-[11.625rem]">
         <SelectGroup className=" h-[11.625rem] mb-[0.75rem]">
-          <SelectItem value="1" className="text-[1.375rem] mt-[0.25rem] ">
-            1반
+          <SelectItem
+            value="frontend"
+            className="text-[1.375rem] mt-[0.25rem] "
+          >
+            프론트엔드
           </SelectItem>
-          <SelectItem value="2" className=" text-[1.375rem] mt-[0.25rem]">
-            2반
+          <SelectItem value="backend" className=" text-[1.375rem] mt-[0.25rem]">
+            백엔드
           </SelectItem>
-          <SelectItem value="3" className="text-[1.375rem] mt-[0.25rem]">
-            3반
+          <SelectItem value="design" className="text-[1.375rem] mt-[0.25rem]">
+            디자인
           </SelectItem>
-          <SelectItem value="4" className="text-[1.375rem] mt-[0.25rem]">
-            4반
+          <SelectItem value="devops" className="text-[1.375rem] mt-[0.25rem]">
+            devops
+          </SelectItem>
+          <SelectItem
+            value="functional group"
+            className="text-[1.375rem] mt-[0.25rem]"
+          >
+            기능반
           </SelectItem>
         </SelectGroup>
       </SelectContent>
