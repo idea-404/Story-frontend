b9c1bc0ca58a8e94d2c052e61e4b51da857983a4
yeondon <s25030@gsm.hs.kr>
2025-11-12 10:24:05 +0900

    feat : zustand 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { Header } from "./components";
-import { useEndpointStore } from "@/store/useEndpointStore";
 
 function App() {
-  const { pathname } = useLocation();
-  const setEndpoint = useEndpointStore((state) => state.setEndpoint);
-
-  useEffect(() => {
-    setEndpoint(pathname);
-  }, [pathname]);
   return (
     <>
       <Header />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,9 @@
 import { Logo, Person, Pen } from "@/assets";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useEndpointStore } from "@/store/useEndpointStore";
 
 const Header = () => {
   const navigate = useNavigate();
-  const { endpoint } = useEndpointStore();
+  const { pathname } = useLocation();
   return (
     <header className="flex justify-center border-b-[0.125rem] border-[#EFF0F2]">
       <div className="flex w-[37.5rem] h-[5.375rem] justify-between items-center">
@@ -12,7 +11,7 @@ const Header = () => {
           <Logo />
         </div>
         <div
-          className={endpoint === "/signin" ? "hidden" : "flex gap-[1.25rem]"}
+          className={pathname === "/signin" ? "hidden" : "flex gap-[1.25rem]"}
         >
           <div
             className="flex items-center gap-[0.625rem] py-[0.375rem] px-[0.875rem] border-[0.025rem] rounded-[1.5rem] font-medium cursor-default
