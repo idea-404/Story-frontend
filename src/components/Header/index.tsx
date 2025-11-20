dbf950763c8b343a7bde1e13fd9cf725f984fda4
yeondon <s25030@gsm.hs.kr>
2025-11-20 17:53:13 +0900

    refactor : 배열 선언 위치 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import { Logo, Person, Pen } from "@/assets";
 import { useLocation, useNavigate } from "react-router-dom";
 
+const nooutput = ["/blog/write", "/portfolio/write"];
+
 const Header = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  const nooutput = ["/blog/write", "/portfolio/write"];
   return (
     <header className="flex justify-center border-b-[0.125rem] border-[#EFF0F2]">
       <div className="flex w-[37.5rem] h-[5.375rem] justify-between items-center">
