8910633734b099cd72a8355a734a808a1f6cac5d
yeondon <s25030@gsm.hs.kr>
2025-11-20 18:02:31 +0900

    refactor : 클릭 가능한 요소 button 으로 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,24 +9,24 @@ const Header = () => {
   return (
     <header className="flex justify-center border-b-[0.125rem] border-[#EFF0F2]">
       <div className="flex w-[37.5rem] h-[5.375rem] justify-between items-center">
-        <div onClick={() => navigate("/")}>
+        <button onClick={() => navigate("/")}>
           <Logo />
-        </div>
+        </button>
         {!(pathname === "/signin") && (
           <div className={"flex gap-[1.25rem]"}>
             {!nooutput.includes(pathname) && (
-              <div
+              <button
                 className={
                   "flex items-center gap-[0.625rem] py-[0.375rem] px-[0.875rem] border-[0.025rem] rounded-[1.5rem] font-medium cursor-pointer text-[#3C3C3E] hover:bg-[#3C3C3E] hover:text-white transition-colors"
                 }
                 onClick={() => navigate("/blog/write")}
               >
                 글 작성 <Pen />
-              </div>
+              </button>
             )}
-            <div onClick={() => navigate("/mypage")}>
+            <button onClick={() => navigate("/mypage")}>
               <Person />
-            </div>
+            </button>
           </div>
         )}
       </div>
