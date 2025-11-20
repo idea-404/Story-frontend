a745812425f2681d7dc88cbb6b9870913eeb68d0
yeondon <s25030@gsm.hs.kr>
2025-11-20 15:35:17 +0900

    feat : includes 처리

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 const Header = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
+  const nooutput = ["/blog/write", "/portfolio/write"];
   return (
     <header className="flex justify-center border-b-[0.125rem] border-[#EFF0F2]">
       <div className="flex w-[37.5rem] h-[5.375rem] justify-between items-center">
@@ -12,9 +13,7 @@ const Header = () => {
         </div>
         {!(pathname === "/signin") && (
           <div className={"flex gap-[1.25rem]"}>
-            {!(
-              pathname === "/portfolio/write" || pathname === "/blog/write"
-            ) && (
+            {!nooutput.includes(pathname) && (
               <div
                 className={
                   "flex items-center gap-[0.625rem] py-[0.375rem] px-[0.875rem] border-[0.025rem] rounded-[1.5rem] font-medium cursor-pointer text-[#3C3C3E] hover:bg-[#3C3C3E] hover:text-white transition-colors"
