46f646f3ea85882c3c8d14c3fc74e0a6182627a1
yeondon <s25030@gsm.hs.kr>
2025-11-20 20:33:21 +0900

    refactor : section 분리

---
+++ src/components/Login/section1.tsx
@@ -0,0 +1,13 @@
+const section1 = () => {
+  return (
+    <div className="w-[32.5rem] my-[5.5rem] text-[2rem] font-medium">
+      당신의
+      <br />
+      <span className="text-primary-main1">Story</span>를
+      <br />
+      시작해 보세요!
+    </div>
+  );
+};
+
+export default section1;
