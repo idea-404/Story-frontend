07ce780ee7d08e543bfc1c8590f94d1fad58a714
yeondon <s25030@gsm.hs.kr>
2025-11-20 22:55:04 +0900

    refactor : section3 분리

---
+++ src/components/Login/section3.tsx
@@ -0,0 +1,24 @@
+import { Line2 } from "@/assets";
+import { Kakao, Google } from "@/assets";
+
+const section3 = () => {
+  return (
+    <div className="flex flex-col items-center gap-[0.5rem]">
+      <div className="flex items-center gap-3 px-[0.625rem] h-[1.5rem]">
+        <Line2 />
+        소셜 로그인
+        <Line2 />
+      </div>
+      <div className="flex gap-[4.5rem]">
+        <button className="bg-[#EFF0F2] rounded-full p-[0.625rem]">
+          <Google />
+        </button>
+        <button className="bg-[#F7E44C] rounded-full p-[0.625rem]">
+          <Kakao />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default section3;
