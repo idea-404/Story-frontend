e4d9f0938e2262e7a4f36e36d31ceafadc2299fb
서연 <s25037@gsm.hs.kr>
2025-11-23 23:14:37 +0900

    feat: 기본 초기 퍼블리싱

---
+++ src/pageContainer/NotFoundPage/index.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <p className="jalnan">404</p>
+      <p className="text-[2.5rem] font-normal text-primary-main1 ">not found</p>
+    </div>
+  );
+};
+
+export default NotFound;
