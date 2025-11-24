b897630c9893f0027b47b403b6aa8da955a5fad6
서연 <s25037@gsm.hs.kr>
2025-11-24 20:09:26 +0900

    feat: 기본 퍼블리싱

--- src/pageContainer/NotFoundPage/index.tsx
+++ src/pageContainer/NotFoundPage/index.tsx
@@ -1,8 +1,18 @@
 const NotFound = () => {
   return (
-    <div style={{ textAlign: "center", padding: "40px" }}>
-      <p className="jalnan">404</p>
-      <p className="text-[2.5rem] font-normal text-primary-main1 ">not found</p>
+    <div
+      style={{
+        textAlign: "center",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: "0 1rem",
+      }}
+    >
+      <img src="/Ola_tired.png" alt="Not Found" className="w-[12rem] mx-auto" />
+      <p className="gap-[0.5rem] jalnan text-[10rem] text-primary-main1">404</p>
+      <p className="text-[2.5rem] font-bold text-primary-main1">not found</p>
     </div>
   );
 };
