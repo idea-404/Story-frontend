ba3d51f1e637576c9d0f3d2e01e5dea70c3bacb0
서연 <s25037@gsm.hs.kr>
2025-11-24 20:19:55 +0900

    feat: style 대신 tailwind로 수정

--- src/pageContainer/NotFoundPage/index.tsx
+++ src/pageContainer/NotFoundPage/index.tsx
@@ -1,15 +1,6 @@
 const NotFound = () => {
   return (
-    <div
-      style={{
-        textAlign: "center",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        padding: "0 1rem",
-      }}
-    >
+    <div className="flex min-h-screen flex-col justify-center px-4 text-center">
       <img src="/Ola_tired.png" alt="Not Found" className="w-[12rem] mx-auto" />
       <p className="gap-[0.5rem] jalnan text-[10rem] text-primary-main1">404</p>
       <p className="text-[2.5rem] font-bold text-primary-main1">not found</p>
