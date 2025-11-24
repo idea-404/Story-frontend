0eab47909d335485d26996a6bccb6865d6fa8000
서연 <s25037@gsm.hs.kr>
2025-11-24 20:32:56 +0900

    style: 불필요한 gap 제거

--- src/pageContainer/NotFoundPage/index.tsx
+++ src/pageContainer/NotFoundPage/index.tsx
@@ -2,7 +2,7 @@ const NotFound = () => {
   return (
     <div className="flex min-h-screen flex-col justify-center px-4 text-center">
       <img src="/Ola_tired.png" alt="Not Found" className="w-[12rem] mx-auto" />
-      <p className="gap-[0.5rem] jalnan text-[10rem] text-primary-main1">404</p>
+      <p className="jalnan text-[10rem] text-primary-main1">404</p>
       <p className="text-[2.5rem] font-bold text-primary-main1">not found</p>
     </div>
   );
