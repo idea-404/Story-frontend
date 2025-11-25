66117cee1d44f748b784b9101f28a1fe23568f7c
yeondon <s25030@gsm.hs.kr>
2025-11-26 08:18:46 +0900

    feat : code 가져오기

--- src/page/Login/index.tsx
+++ src/page/Login/index.tsx
@@ -1,6 +1,16 @@
 import { LoginSection1, LoginSection2, LoginSection3 } from "@/components";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const Login = () => {
+  const { search } = useLocation();
+  useEffect(() => {
+    const params = new URLSearchParams(search);
+    const code = params.get("code");
+
+    console.log("카카오 인가코드:", code);
+  }, [search]);
+
   return (
     <main className="flex items-center flex-col">
       <LoginSection1 />
