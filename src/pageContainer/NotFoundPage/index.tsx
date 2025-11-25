5f1d86eaad53cda50387481e2448f286bb833aec
서연 <s25037@gsm.hs.kr>
2025-11-25 10:01:14 +0900

    style: 텍스트 크기 조정

--- src/pageContainer/NotFoundPage/index.tsx
+++ src/pageContainer/NotFoundPage/index.tsx
@@ -1,9 +1,9 @@
 const NotFound = () => {
   return (
     <div className="flex min-h-screen flex-col justify-center px-4 text-center">
-      <img src="/Ola_tired.png" alt="Not Found" className="w-[12rem] mx-auto" />
-      <p className="jalnan text-[10rem] text-primary-main1">404</p>
-      <p className="text-[2.5rem] font-bold text-primary-main1">not found</p>
+      <img src="/Ola_tired.png" alt="Not Found" className="w-[10rem] mx-auto" />
+      <p className="jalnan text-[8rem] text-primary-main1">404</p>
+      <p className="text-[1.5rem] font-bold text-primary-main1">not found</p>
     </div>
   );
 };
