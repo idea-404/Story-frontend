19e6916a33c45fd398c3a0eff85fc530e256a881
yeondon <s25030@gsm.hs.kr>
2025-11-27 08:07:06 +0900

    feat : 토큰 형식 받아오기

--- src/Store/token.ts
+++ src/Store/token.ts
@@ -1,13 +1,49 @@
 import { create } from "zustand";
-interface TokenState {
-  token: string | null;
-  setToken: (token: string) => void;
-  clearToken: () => void;
+import { jwtDecode } from "jwt-decode";
+//토큰 타입 정의
+interface DecodedToken {
+  sub: string;
+  userId: number;
+  nickname: string;
+  studentId: number;
+  profileImage: string;
+  major: string;
+  introduce: string;
+  role: string;
+}
+//zustand 타입정의
+interface AuthState {
+  auth: {
+    token: string | null;
+    user: DecodedToken | null;
+  };
+  setAuthWithToken: (token: string) => void;
+  clearAuth: () => void;
 }
 
-const useTokenStore = create<TokenState>((set) => ({
-  token: null,
-  setToken: (token) => set({ token }),
-  clearToken: () => set({ token: null }),
+const useTokenStore = create<AuthState>((set) => ({
+  auth: { token: null, user: null },
+  setAuthWithToken: (token: string) => {
+    try {
+      const decoded: DecodedToken = jwtDecode(token);
+      set({
+        auth: {
+          token: token,
+          user: decoded,
+        },
+      });
+    } catch (err) {
+      console.error("JWT decode error:", err);
+      set({
+        auth: { token: null, user: null },
+      });
+    }
+  },
+
+  clearAuth: () => {
+    set({
+      auth: { token: null, user: null },
+    });
+  },
 }));
 export default useTokenStore;
