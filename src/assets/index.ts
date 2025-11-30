71339e392deecfa41e9a074f2fa5e11297a0f7b4
yeondon <s25030@gsm.hs.kr>
2025-11-30 21:23:11 +0900

    feat : Check 이모지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,4 @@ export { default as Pen } from "./Pen";
 export { default as Google } from "./Google";
 export { default as Kakao } from "./Kakao";
 export { default as Line2 } from "./Line2";
+export { default as Check } from "./Check";
