18c0ff071a5e75e3510c40173f6390e85cc7cd36
서연 <s25037@gsm.hs.kr>
2025-11-30 22:46:55 +0900

    fix: prettier 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as Header } from './Header';
-export { default as MainCard } from './MainCard';
-export { default as MainHeader } from './MainHeader';
+export { default as Header } from "./Header";
+export { default as MainCard } from "./MainCard";
+export { default as MainHeader } from "./MainHeader";
