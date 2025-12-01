cc46430ca23bfb84387cf67ade362fbf26e4a7a5
yeondon <s25030@gsm.hs.kr>
2025-12-01 22:58:43 +0900

    fix : import 변경

--- src/API/tokenCode.ts
+++ src/API/tokenCode.ts
@@ -1,12 +1,12 @@
 import type { oauthType } from "@/Types";
-import setToken from "@/Store/token";
+import useTokenStore from "@/Store/token";
 import axios from "axios";
 
 export const TokenCode = async (code: string, type: oauthType) => {
   try {
     const res = await axios.post(`/v1/auth/${type}`, { code: code });
     const tokenValue = res.data.token;
-    setToken(tokenValue);
+    useTokenStore.getState().setAuthWithToken(tokenValue);
     // console.log("받은 토큰:", tokenValue);
   } catch (error) {
     console.error("Error fetching token:", error);
