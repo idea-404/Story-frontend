74714d99356a8af7d08ac52c4725dfc3986ca7bd
yeondon <s25030@gsm.hs.kr>
2025-12-01 23:01:19 +0900

    fix : baseURL import 변경

--- src/API/tokenCode.ts
+++ src/API/tokenCode.ts
@@ -1,10 +1,10 @@
 import type { oauthType } from "@/Types";
 import useTokenStore from "@/Store/token";
-import axios from "axios";
+import api from "./api";
 
 export const TokenCode = async (code: string, type: oauthType) => {
   try {
-    const res = await axios.post(`/v1/auth/${type}`, { code: code });
+    const res = await api.post(`/v1/auth/${type}`, { code: code });
     const tokenValue = res.data.token;
     useTokenStore.getState().setAuthWithToken(tokenValue);
     // console.log("받은 토큰:", tokenValue);
