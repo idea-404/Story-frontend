18bcc81710318e238c44455c5d149d6a871c0c5b
yeondon <s25030@gsm.hs.kr>
2025-12-18 08:45:26 +0900

    feat : header 설정

--- src/components/Print/Printheader/index.tsx
+++ src/components/Print/Printheader/index.tsx
@@ -1,5 +1,14 @@
+import { Person } from "@/assets";
+
 const Printheader = () => {
-  return <div>ㅁㄴㅇㄹ</div>;
+  return (
+    <div className="flex items-center gap-[0.96rem] py-[1.63rem]">
+      <Person h={72} />
+      <div className="text-black font-pretendard text-[1.25rem] font-medium leading-normal tracking-[0.05rem]">
+        1110 이남진
+      </div>
+    </div>
+  );
 };
 
 export default Printheader;
