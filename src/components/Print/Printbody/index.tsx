748276eec4b3bdb7d9e4cd1a71d76444440675dc
yeondon <s25030@gsm.hs.kr>
2025-12-17 23:41:00 +0900

    feat : body 유동적 변경

--- src/components/Print/Printbody/index.tsx
+++ src/components/Print/Printbody/index.tsx
@@ -1,10 +1,10 @@
-const Printbody = () => {
+const Printbody = ({ body = "내용을 입력해 주세요." }: { body?: string }) => {
   const buttonStyle =
     "py-[0.47rem] px-[2.03rem] bg-[#EFF0F2] rounded-[0.9375rem] text-primary-main1 font-bold";
   return (
     <div className="flex flex-col gap-[1rem]">
       <div className="w-[35.75rem] h-[40rem] mt-[3.75rem] overflow-y-scroll">
-        내용이 들어가는 부분입니다.
+        {body}
       </div>
       <div className="flex justify-end gap-[2.25rem]">
         <button className={buttonStyle}>임시저장</button>
