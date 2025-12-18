5c39b14077ed79c1f58248b22e27ecb704094647
yeondon <s25030@gsm.hs.kr>
2025-12-18 09:02:41 +0900

    feat : 반응형으로 수정

--- src/components/Print/Printbody/index.tsx
+++ src/components/Print/Printbody/index.tsx
@@ -3,7 +3,7 @@ const Printbody = ({ body = "내용을 입력해 주세요." }: { body?: string 
     "py-[0.47rem] px-[2.03rem] bg-[#EFF0F2] rounded-[0.9375rem] text-primary-main1 font-bold";
   return (
     <div className="flex flex-col gap-[1rem]">
-      <div className="w-[35.75rem] h-[40rem] mt-[3.75rem] overflow-y-scroll">
+      <div className="w-[35.75rem] h-[59.26vh] mt-[3.75rem] overflow-y-scroll">
         {body}
       </div>
       <div className="flex justify-end gap-[2.25rem]">
--- src/components/Print/Printheader/index.tsx
+++ src/components/Print/Printheader/index.tsx
@@ -4,7 +4,7 @@ const Printheader = () => {
   return (
     <div className="flex items-center gap-[0.96rem] py-[1.63rem]">
       <Person h={72} />
-      <div className="text-black font-pretendard text-[1.25rem] font-medium leading-normal tracking-[0.05rem]">
+      <div className="text-black font-pretendard text-[1.25rem] ">
         1110 이남진
       </div>
     </div>
