afe4bc3cda844cacbb2f8416688ef8523195ae28
yeondon <s25030@gsm.hs.kr>
2025-12-18 09:48:16 +0900

    fix : 구조 또 또 수정

--- src/components/Print/index.tsx
+++ src/components/Print/index.tsx
@@ -1,12 +1,6 @@
 import Printtitle from "./Printtitle";
 import Printbody from "./Printbody";
-const Print = ({
-  title,
-  body = "내용을 입력해 주세요.",
-}: {
-  title: string;
-  body?: string;
-}) => {
+const Print = ({ title, body }: { title: string; body: string }) => {
   return (
     <div className="mt-[4.25vh]">
       <Printtitle title={title} />
