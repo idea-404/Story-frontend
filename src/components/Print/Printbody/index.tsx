61b2c38d8c1264a7b8d6b48f1271c07ab422f48c
yeondon <s25030@gsm.hs.kr>
2025-12-18 09:14:47 +0900

    fix : 디자인 변경에 따른 갭 수정

--- src/components/Print/Printbody/index.tsx
+++ src/components/Print/Printbody/index.tsx
@@ -2,7 +2,7 @@ const Printbody = ({ body = "내용을 입력해 주세요." }: { body?: string 
   const buttonStyle =
     "py-[0.47rem] px-[2.03rem] bg-[#EFF0F2] rounded-[0.9375rem] text-primary-main1 font-bold";
   return (
-    <div className="flex flex-col gap-[1rem]">
+    <div className="flex flex-col gap-[2.75rem]">
       <div className="w-[35.75rem] h-[59.26vh] mt-[3.75rem] overflow-y-scroll">
         {body}
       </div>
