a8f13327847ae01e9a9b022c4487f4716adc6dfd
yeondon <s25030@gsm.hs.kr>
2025-12-18 15:04:23 +0900

    feat : Header 완성

--- src/components/Inputheader/index.tsx
+++ src/components/Inputheader/index.tsx
@@ -1,13 +1,42 @@
 import HeadingSelect from "@/assets/select/HeadingSelect";
+import { Line3, Line4, Dotline, Quotes, Code, Link, Image } from "@/assets";
 const Inputheader = () => {
   return (
-    <div className="flex">
+    <div className="flex gap-[0.55rem] items-center ">
       <div>
         <HeadingSelect />
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <Line4 />
+      <div className="flex gap-[1.88rem] px-[0.74rem]">
+        <button className="font-bold">B</button>
+        <button className="italic">I</button>
+        <button className="underline">U</button>
+        <button className="line-through">S</button>
+      </div>
+      <Line4 />
+      <div className="flex gap-[1.88rem] items-center px-[0.55rem]">
+        <button>
+          <Line3 />
+        </button>
+        <button>
+          <Dotline />
+        </button>
+        <button>
+          <Code />
+        </button>
+        <button>
+          <Quotes />
+        </button>
+      </div>
+      <Line4 />
+      <div className="flex gap-[1.88rem] px-[0.89rem]">
+        <button>
+          <Link />
+        </button>
+        <button>
+          <Image />
+        </button>
+      </div>
     </div>
   );
 };
