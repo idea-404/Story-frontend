65e1d23b13a2ebb90b7c2152a40f99dbde8aec48
yeondon <s25030@gsm.hs.kr>
2025-12-18 09:12:57 +0900

    feat : 해더 삭제

--- src/components/Print/index.tsx
+++ src/components/Print/index.tsx
@@ -1,10 +1,8 @@
 import Printtitle from "./Printtitle";
 import Printbody from "./Printbody";
-import Printheader from "./Printheader";
 const Print = () => {
   return (
     <div className="">
-      <Printheader />
       <Printtitle />
       <Printbody />
     </div>
