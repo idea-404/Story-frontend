6ec63d496a9dc9efac7c7afa92bcde27c16ca422
서연 <s25037@gsm.hs.kr>
2025-12-18 17:48:46 +0900

    remove: 테스트 모달 페이지 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,6 @@ import MainPage from "@/page/MainPage";
 import { Login } from "./page";
 import NotFound from "./pageContainer/NotFoundPage";
 import { Signin } from "@/page";
-import ResendMail from "./components/Modal/ResendMail";
-import Delete from "./components/Modal/Delete";
-import AIFeedback from "./components/Modal/AIFeedback";
-import SelectWrite from "./components/Modal/SelectWrite";
-import Portfolio from "./components/Modal/Portfolio";
-import Blog from "./components/Modal/blog";
 
 function App() {
   const { pathname } = useLocation();
@@ -26,12 +20,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/info" element={<Signin />} />
-        <Route path="/modal" element={<ResendMail />} />
-        <Route path="/delete" element={<Delete />} />
-        <Route path="/feedback" element={<AIFeedback />} />
-        <Route path="/selectwrite" element={<SelectWrite />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/blog" element={<Blog />} />
         <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
