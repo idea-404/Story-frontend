bc78753c0f15483cc24c3e015b218b5985dd36ab
yeondon <s25030@gsm.hs.kr>
2025-12-18 09:12:48 +0900

    feat : 경계선 크기 조절

--- src/components/Print/Printtitle/index.tsx
+++ src/components/Print/Printtitle/index.tsx
@@ -4,7 +4,7 @@ const Printtitle = ({ title = "제목을 입력하세요" }: { title?: string })
       <h1 className="text-[#3C3C3E] text-[1.875rem] font-medium leading-normal pb-[0.87rem]">
         {title}
       </h1>
-      <hr className="h-[0.125rem] bg-[#CBCCCE] w-[37.5rem]" />
+      <hr className="h-[0.125rem] bg-[#CBCCCE] w-[35.75rem]" />
     </div>
   );
 };
