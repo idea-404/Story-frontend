8a2eff026d247f077ca009fc0ec4f65e806c7c9a
yeondon <s25030@gsm.hs.kr>
2025-12-22 23:21:35 +0900

    feat : api 설정 로직

---
+++ src/API/aireq.ts
@@ -0,0 +1,31 @@
+import api from "@/API/api";
+import type { conversionType } from "@/Types";
+export const aireq = async (
+  body: string,
+  id: number,
+  setConversionType: React.Dispatch<React.SetStateAction<conversionType>>
+) => {
+  try {
+    const res = await api.post(`/${id}`, {
+      question: body,
+    });
+    setConversionType("ing");
+
+    if (res.status === 200) {
+      setConversionType("Ok");
+    } else if (res.status === 400) {
+      alert("다시시도 해주세요.");
+      setConversionType("failed");
+    } else if (res.status === 401) {
+      setConversionType("failed");
+      alert("로그인 후 이용해주세요.");
+    } else if (res.status === 429) {
+      setConversionType("failed");
+      alert("영역을 다시 설정해주세요.");
+    }
+  } catch (error) {
+    setConversionType("failed");
+    console.error("포트폴리오 저장 오류:", error);
+    alert("백엔드 서버 비상!!!!!!!!!");
+  }
+};
