c0b920a734493f70bfe26ea074df701f0b07fa0c
yeondon <s25030@gsm.hs.kr>
2025-12-22 13:56:47 +0900

    feat : api 포폴 저장 로직

--- src/API/index.ts
+++ src/API/index.ts
@@ -2,3 +2,4 @@ export * from "./kakao";
 export * from "./google";
 export * from "./tokenCode";
 export * from "./info";
+export * from "./portfolio";
---
+++ src/API/portfolio.ts
@@ -0,0 +1,13 @@
+import api from "@/API/api";
+export const write = async (title: string, content: string) => {
+  const res = await api.post(`/portfolio/write`, { title, content });
+  if (res.status === 201) {
+    alert("포트폴리오가 성공적으로 저장되었습니다.");
+  } else if (res.status === 400) {
+    alert("제목이나 내용 추가 후 다시 시도해주세요.");
+  } else if (res.status === 401) {
+    alert("로그인 후 이용해주세요.");
+  } else {
+    alert("백엔드 서버 비상!!!!!!!!!");
+  }
+};
