26b03cbca2a0b94361b26efda5be2bb0b1364718
yeondon <s25030@gsm.hs.kr>
2025-12-22 23:59:59 +0900

    feat : hook 문제 해결

--- src/components/AiPrint/Ok/index.tsx
+++ src/components/AiPrint/Ok/index.tsx
@@ -3,8 +3,9 @@ import remarkGfm from "remark-gfm";
 import rehypeSanitize from "rehype-sanitize";
 import { useAiResponseStore } from "@/Store/AiResponse";
 
-const { response } = useAiResponseStore();
 const Ok = () => {
+  const { response } = useAiResponseStore();
+
   return (
     <div className="mt-[1.25rem] ml-[7.5rem] w-[35rem] h-[75.93vh] rounded-[1.25rem] shadow-[-0.5px_-0.5px_20px_0.1px_#EFF0F2,0.5px_0.5px_20px_0.1px_#EFF0F2] flex flex-col items-center justify-center">
       <ReactMarkdown
