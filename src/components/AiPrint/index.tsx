77cceb50ca82a4c741652dcc00c03b5c2dd07348
yeondon <s25030@gsm.hs.kr>
2025-12-22 10:59:43 +0900

    feat : 올라 추가

--- src/components/AiPrint/index.tsx
+++ src/components/AiPrint/index.tsx
@@ -1,4 +1,13 @@
+import { Back } from "@/assets";
+
 const AiPrint = () => {
-  return <div>AI Component</div>;
+  return (
+    <div className="w-[35rem] h-[75.93vh] rounded-[1.25rem] shadow-[-0.5px_-0.5px_20px_0.1px_#EFF0F2,0.5px_0.5px_20px_0.1px_#EFF0F2] flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <Back />
+        <img src="Ola_happy.png" className="w-[7.8125rem] h-[8.75rem]" />
+      </div>
+    </div>
+  );
 };
 export default AiPrint;
