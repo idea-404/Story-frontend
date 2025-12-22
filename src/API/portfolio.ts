e81743660c6465ace0fef1db668e6531d8dcff56
yeondon <s25030@gsm.hs.kr>
2025-12-22 17:01:26 +0900

    feat : try 문으로 변경

--- src/API/portfolio.ts
+++ src/API/portfolio.ts
@@ -1,13 +1,17 @@
 import api from "@/API/api";
+
 export const write = async (title: string, content: string) => {
-  const res = await api.post(`/portfolio/write`, { title, content });
-  if (res.status === 201) {
-    alert("포트폴리오가 성공적으로 저장되었습니다.");
-  } else if (res.status === 400) {
-    alert("제목이나 내용 추가 후 다시 시도해주세요.");
-  } else if (res.status === 401) {
-    alert("로그인 후 이용해주세요.");
-  } else {
+  try {
+    const res = await api.post(`/portfolio/write`, { title, content });
+    if (res.status === 201) {
+      alert("포트폴리오가 성공적으로 저장되었습니다.");
+    } else if (res.status === 400) {
+      alert("제목이나 내용 추가 후 다시 시도해주세요.");
+    } else if (res.status === 401) {
+      alert("로그인 후 이용해주세요.");
+    }
+  } catch (error) {
+    console.error("포트폴리오 저장 오류:", error);
     alert("백엔드 서버 비상!!!!!!!!!");
   }
 };
