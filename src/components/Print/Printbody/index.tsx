d307e4e33c90453889f34245fc8b6fecd55a4baf
yeondon <s25030@gsm.hs.kr>
2025-12-23 08:05:28 +0900

    feat : 버튼 조건부 랜더링

--- src/components/Print/Printbody/index.tsx
+++ src/components/Print/Printbody/index.tsx
@@ -13,6 +13,11 @@ const Printbody = ({
 }) => {
   const buttonStyle =
     "py-[0.47rem] px-[2.03rem] bg-[#EFF0F2] rounded-[0.9375rem] text-primary-main1 font-bold";
+  const writeType = location.pathname.includes("/blog/write")
+    ? "blog"
+    : location.pathname.includes("/portfolio/write")
+    ? "portfolio"
+    : "unknown";
   return (
     <div className="flex flex-col gap-[2.75rem]">
       <div className="w-[35.75rem] h-[59.26vh] mt-[1rem] overflow-y-scroll overflow-x-hidden prose">
@@ -24,12 +29,14 @@ const Printbody = ({
         </ReactMarkdown>
       </div>
       <div className="flex justify-end gap-[2.25rem]">
-        <button
-          onClick={() => write(title, body, body.slice(0, 64))}
-          className={buttonStyle}
-        >
-          임시저장
-        </button>
+        {writeType === "portfolio" && (
+          <button
+            onClick={() => write(title, body, body.slice(0, 64))}
+            className={buttonStyle}
+          >
+            임시저장
+          </button>
+        )}
         <button onClick={() => setEnd(true)} className={buttonStyle}>
           작성완료
         </button>
