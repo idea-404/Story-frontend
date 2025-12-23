35e404c07e291dbec570815e6fe9051bf8441fab
yeondon <s25030@gsm.hs.kr>
2025-12-23 14:45:35 +0900

    feat : 댓글창 해더

--- src/page/commant/Printheader/index.tsx
+++ src/page/commant/Printheader/index.tsx
@@ -2,11 +2,12 @@ import { Person } from "@/assets";
 
 const Printheader = () => {
   return (
-    <div className="flex items-center gap-[0.96rem] py-[1.63rem]">
-      <Person h={72} />
-      <div className="text-black font-pretendard text-[1.25rem] ">
-        1110 이남진
+    <div className="flex justify-between w-[37.5rem] mt-[2rem] mb-[1.75rem]">
+      <div className="flex items-center gap-[0.96rem]">
+        <Person h={72} />
+        <div className="text-black text-[1.25rem]">1110 이남진</div>
       </div>
+      <div className="flex items-center text-[#828387]">2025-20-20</div>
     </div>
   );
 };
