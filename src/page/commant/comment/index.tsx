34b4a78a10f08c78c5e13dd88d6a0186964282f2
yeondon <s25030@gsm.hs.kr>
2025-12-23 15:13:26 +0900

    feat : text area 새엇ㅇ

--- src/page/commant/comment/index.tsx
+++ src/page/commant/comment/index.tsx
@@ -1,9 +1,11 @@
-const Commant = () => {
+const Commantintput = () => {
   return (
-    <div className="flex flex-col w-[32rem]">
-      <div></div>
-      <div> </div>
+    <div className="flex flex-col justify-center w-[32rem]">
+      <div className="w-[32rem] text-[1.25rem]">10개의 댓글</div>
+      <div className="w-[32rem] h-[7.25rem]">
+        <textarea></textarea>
+      </div>
     </div>
   );
 };
-export default Commant;
+export default Commantintput;
