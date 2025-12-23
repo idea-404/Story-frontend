2a6b20dbdef48e7902683edbae934cc7455630e7
yeondon <s25030@gsm.hs.kr>
2025-12-23 14:55:30 +0900

    feat : 조회 추가

--- src/page/commant/index.tsx
+++ src/page/commant/index.tsx
@@ -17,13 +17,14 @@ const Commant = () => {
       </button>
       <Printheader />
       <div>
-        <h1 className="text-[#3C3C3E] text-[1.875rem] font-medium leading-normal pb-[0.87rem]">
-          {title}
+        <h1 className="flex justify-between text-[#3C3C3E] text-[1.875rem] font-medium leading-normal pb-[0.87rem]">
+          <div>{title}</div>
+          <div className="text-[1rem] text-[#828387]">조회 12</div>
         </h1>
         <hr className="h-[0.125rem] bg-[#CBCCCE] w-[35.75rem]" />
       </div>
       <div className="flex flex-col gap-[2.75rem]">
-        <div className="w-[35.75rem] h-[59.26vh] mt-[1rem] overflow-y-scroll overflow-x-hidden prose">
+        <div className="w-[35.75rem] h-[59.26vh] mt-[1rem] prose">
           <ReactMarkdown
             remarkPlugins={[remarkGfm]}
             rehypePlugins={[rehypeSanitize]}
