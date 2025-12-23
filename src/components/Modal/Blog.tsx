24ae65f3d160c496692580389aca1ef7c2aa51e4
yeondon <s25030@gsm.hs.kr>
2025-12-23 10:57:43 +0900

    feat : 이미지 선택 로직

--- src/components/Modal/Blog.tsx
+++ src/components/Modal/Blog.tsx
@@ -15,13 +15,28 @@ export default function Blog({
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
+      <input
+        type="file"
+        className="hidden"
+        id="imageUpload"
+        onChange={async (e) => {
+          const file = e.target.files?.[0];
+          if (file) {
+          }
+        }}
+      />
       <div className="flex w-[560px] rounded-3xl bg-white p-6">
         <div className="flex w-1/2 flex-col items-center justify-center rounded-2xl bg-gray-100 p-5">
           <PublishBlog className="ml-35" />
 
-          <div className="mt-4 rounded-xl bg-primary-main1 px-4 py-2 text-center text-sm text-white">
+          <button
+            onClick={() => {
+              document.getElementById("imageUpload")?.click();
+            }}
+            className="mt-4 rounded-xl bg-primary-main1 px-4 py-2 text-center text-sm text-white"
+          >
             썸네일 선택하기
-          </div>
+          </button>
         </div>
 
         <div className="ml-4 flex w-1/2 flex-col">
