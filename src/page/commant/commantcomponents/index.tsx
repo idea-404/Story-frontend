6b42f1d8459ea197976a56acf848e0c0a09b949e
yeondon <s25030@gsm.hs.kr>
2025-12-23 15:59:46 +0900

    feat : 반복문 랜더링

--- src/page/commant/commantcomponents/index.tsx
+++ src/page/commant/commantcomponents/index.tsx
@@ -1,16 +1,33 @@
 import { Person } from "@/assets";
 
-const Commantcomponents = () => {
+interface Comment {
+  comment_id: number;
+  user_id: number;
+  content: string;
+  createAt: string;
+}
+
+interface CommantcomponentsProps {
+  comments: Comment[];
+}
+
+const Commantcomponents = ({ comments }: CommantcomponentsProps) => {
   return (
     <div className="flex flex-col">
-      <div className="flex justify-between w-[32rem] mt-[3.75rem] mb-[1.25rem]">
-        <div className="flex items-center gap-[0.96rem]">
-          <Person h={40} />
-          <div className="text-black text-[1.25rem]">1110 이남진</div>
+      {comments.map((comment) => (
+        <div key={comment.comment_id} className="mb-[2rem]">
+          <div className="flex justify-between w-[32rem] mt-[3.75rem] mb-[1.25rem]">
+            <div className="flex items-center gap-[0.96rem]">
+              <Person h={40} />
+              <div className="text-black text-[1.25rem]">{comment.user_id}</div>
+            </div>
+            <div className="flex items-center text-[#828387]">
+              {comment.createAt}
+            </div>
+          </div>
+          <div className="w-[32rem] px-[1rem]">{comment.content}</div>
         </div>
-        <div className="flex items-center text-[#828387]">2025-20-20</div>
-      </div>
-      <div className="w-[32rem] px-[1rem]"></div>
+      ))}
     </div>
   );
 };
