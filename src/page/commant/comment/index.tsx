fd38ccd362a365c8281150adc0a80c333cfd19ef
yeondon <s25030@gsm.hs.kr>
2025-12-23 15:21:08 +0900

    feat : textarea 크기 설정

--- src/page/commant/comment/index.tsx
+++ src/page/commant/comment/index.tsx
@@ -2,8 +2,12 @@ const Commantintput = () => {
   return (
     <div className="flex flex-col justify-center w-[32rem]">
       <div className="w-[32rem] text-[1.25rem]">10개의 댓글</div>
-      <div className="w-[32rem] h-[7.25rem]">
-        <textarea></textarea>
+      <div className="w-[32rem] h-[7.25rem] bg-[#EFF0F2] rounded-[0.4375rem] mt-[0.5rem] px-[1rem] pt-[1rem]">
+        <textarea
+          placeholder="댓글을 입력하세요."
+          className="h-[4rem] w-[30rem]"
+        />
+        <div className="flex justify-end">asdf</div>
       </div>
     </div>
   );
