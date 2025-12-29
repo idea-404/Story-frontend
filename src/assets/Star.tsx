215b8aebff8225f1b9f785d1bcc0ef255f5b9cfd
서연 <s25037@gsm.hs.kr>
2025-12-29 09:53:51 +0900

    fix: 다시 svg 크기 수정

--- src/assets/Star.tsx
+++ src/assets/Star.tsx
@@ -1,8 +1,8 @@
 const Star = () => {
   return (
     <svg
-      width="160"
-      height="160"
+      width="30"
+      height="30"
       viewBox="0 0 14 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
